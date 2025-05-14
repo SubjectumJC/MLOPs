93c904f061e09d79edc8ff54257ce9292e2b5d2c
AlejandroJC <ajcr990308@gmail.com>
2025-05-13 22:44:33 -0500

    Ajuste directorio

--- proyecto3/dags/scripts/run_training.py
+++
@@ -1,4 +0,0 @@
-import subprocess, os, sys, pathlib
-print(">> Ejecutando entrenamiento local")
-script_path = pathlib.Path(__file__).parent / "train_local" / "train.py"
-subprocess.check_call([sys.executable, "-u", str(script_path)])
--- proyecto3/k8s/README.md
+++
@@ -1,7 +0,0 @@
-Manifiestos generados con `kompose convert -f ../docker-compose.yml`.  
-Aplícalos con:
-
-```bash
-kubectl apply -k .
-```
-
